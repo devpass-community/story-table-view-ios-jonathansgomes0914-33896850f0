bd425cc35923b300262dcc09035c7d893ae47bac
Jonathan Gomes <jonathangomes.ti@gmail.com>
2023-04-25 13:14:10 -0300

    adds datasource main view

--- EvercodeApp/Screens/Main/MainView.swift
+++ EvercodeApp/Screens/Main/MainView.swift
@@ -6,6 +6,8 @@ final class MainView: UIView {
        
         let view = UITableView()
         view.translatesAutoresizingMaskIntoConstraints = false
+        view.dataSource = self
+        view.register(UITableViewCell.self, forCellReuseIdentifier: "basicStyle")
         return view
     }()
     
@@ -57,4 +59,19 @@ private extension MainView {
             tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
     }
+}
+
+extension MainView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "basicStyle", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    
 }
